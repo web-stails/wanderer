dd9e51c7414aac38d02ad97ce35a3244894eb717
web-stails <web.stails@gmail.com>
2021-03-15 14:13:30 +0300

    добавлены типы полей

--- library/convert_sf_data.js
+++ library/convert_sf_data.js
@@ -12,6 +12,7 @@ module.exports = {
 						tp_pr = params_field[0] || params_field;
 						data_param = params_field[1] || false;
 					}
+					/* bool */
 
 					if(typeof tp_pr === 'string' && tp_pr === 'bool') {
 						tables[name_table][name_field] = {
@@ -43,14 +44,20 @@ module.exports = {
 						}
 					}
 
+
+					/* id */
+
 					if(typeof tp_pr === 'string' && tp_pr === 'id') {
 						tables[name_table][name_field] = {
-							type: 'int',
+							type: 'big_int',
 							default: 0,
 							index: true,
+							unsigned: true,
 							param: data_param
 						}
 					}
+
+					/* integer */
 
 					if(typeof tp_pr === 'string' && tp_pr === 'int') {
 						tables[name_table][name_field] = {
@@ -82,6 +89,42 @@ module.exports = {
 						}
 					}
 
+
+					if(typeof tp_pr === 'string' && tp_pr === 'unsigned_int') {
+						tables[name_table][name_field] = {
+							type: 'int',
+							default: 0,
+							unsigned: true,
+							param: data_param
+						}
+					}
+
+					if(typeof tp_pr === 'string' && tp_pr === 'unsigned_int_index') {
+						tables[name_table][name_field] = {
+							type: 'int',
+							default: 0,
+							index: true,
+							unsigned: true,
+							param: data_param
+						}
+					}
+
+					let unsigned_reg_int = /^unsigned_int\((\d+)\)(_index)?$/;
+					if(typeof tp_pr === 'string' && unsigned_reg_int.test(tp_pr)) {
+						let param_constraint = tp_pr.match(unsigned_reg_int);
+
+						tables[name_table][name_field] = {
+							type: 'int',
+							default: 0,
+							unsigned: true,
+							constraint: parseInt(param_constraint[1]),
+							index: isset(param_constraint[2]),
+							param: data_param
+						}
+					}
+
+					/* varchar */
+
 					if(typeof tp_pr === 'string' && (tp_pr === 'varchar' || tp_pr === 'string')) {
 						tables[name_table][name_field] = {
 							type: 'varchar',
@@ -99,6 +142,8 @@ module.exports = {
 							param: data_param
 						}
 					}
+
+
 					let reg_varchar = /^(?:varchar|string)\((\d+)\)(_index)?$/;
 
 					if(typeof tp_pr === 'string' && reg_varchar.test(tp_pr)) {
@@ -113,6 +158,8 @@ module.exports = {
 						}
 					}
 
+					/* text */
+
 					if(typeof tp_pr === 'string' && tp_pr === 'text') {
 						tables[name_table][name_field] = {
 							type: 'text',
@@ -121,6 +168,56 @@ module.exports = {
 							param: data_param
 						}
 					}
+
+					if(typeof tp_pr === 'string' && tp_pr == 'longtext') {
+						tables[name_table][name_field] = {
+							type: 'longtext',
+							null: true,
+							param: data_param
+						}
+					}
+					if(typeof tp_pr === 'string' && tp_pr == 'longText') {
+						tables[name_table][name_field] = {
+							type: 'longtext',
+							null: true,
+							param: data_param
+						}
+					}
+					if(typeof tp_pr === 'string' && tp_pr === 'long_text') {
+						tables[name_table][name_field] = {
+							type: 'longtext',
+							default: '',
+							null: true,
+							param: data_param
+						}
+					}
+
+					if(typeof tp_pr === 'string' && tp_pr == 'smalltext') {
+						tables[name_table][name_field] = {
+							type: 'longtext',
+							null: true,
+							param: data_param
+						}
+					}
+					if(typeof tp_pr === 'string' && tp_pr == 'longText') {
+						tables[name_table][name_field] = {
+							type: 'longtext',
+							null: true,
+							param: data_param
+						}
+					}
+					if(typeof tp_pr === 'string' && tp_pr === 'long_text') {
+						tables[name_table][name_field] = {
+							type: 'longtext',
+							default: '',
+							null: true,
+							param: data_param
+						}
+					}
+
+
+					/* decimal */
+
 
 					if(typeof tp_pr === 'string' && tp_pr == 'decimal') {
 						tables[name_table][name_field] = {
@@ -141,6 +238,27 @@ module.exports = {
 						}
 					}
 
+					if(typeof tp_pr === 'string' && tp_pr == 'unsigned_decimal') {
+						tables[name_table][name_field] = {
+							type: 'decimal',
+							constraint: '10,2',
+							default: 0,
+							unsigned: true,
+							param: data_param
+						}
+					}
+
+					if(typeof tp_pr === 'string' && tp_pr == 'unsigned_decimal_index') {
+						tables[name_table][name_field] = {
+							type: 'decimal',
+							constraint: '10,2',
+							default: 0,
+							index: true,
+							unsigned: true,
+							param: data_param
+						}
+					}
+
 					let reg_decimal = /^decimal\(([^\)]+)\)(_index)?$/;
 
 					if(typeof tp_pr === 'string' && reg_decimal.test(tp_pr)) {
@@ -155,6 +273,23 @@ module.exports = {
 						}
 					}
 
+					let unsigned_reg_decimal = /^decimal\(([^\)]+)\)(_index)?$/;
+
+					if(typeof tp_pr === 'string' && unsigned_reg_decimal.test(tp_pr)) {
+						let param_constraint = tp_pr.match(unsigned_reg_decimal);
+
+						tables[name_table][name_field] = {
+							type: 'decimal',
+							default: 0,
+							constraint: parseInt(param_constraint[1]),
+							index: isset(param_constraint[2]),
+							unsigned: true,
+							param: data_param
+						}
+					}
+
+					/* lson */
+
 					if(typeof tp_pr === 'string' && tp_pr == 'json') {
 						tables[name_table][name_field] = {
 							type: 'json',
@@ -163,6 +298,7 @@ module.exports = {
 						}
 					}
 
+					/* timestamp */
 
 					if(typeof tp_pr === 'string' && tp_pr == 'timestamp') {
 						tables[name_table][name_field] = {
@@ -171,6 +307,15 @@ module.exports = {
 							param: data_param
 						}
 					}
+
+					// if(typeof tp_pr === 'string' && tp_pr == 'unsigned_timestamp') {
+					// 	tables[name_table][name_field] = {
+					// 		type: 'timestamp',
+					// 		// null:true,
+					// 		unsigned: true,
+					// 		param: data_param
+					// 	}
+					// }
 
 					if(typeof tp_pr === 'string' && tp_pr == 'date') {
 						tables[name_table][name_field] = {
@@ -196,13 +341,7 @@ module.exports = {
 						}
 					}
 
-					if(typeof tp_pr === 'string' && tp_pr == 'longtext') {
-						tables[name_table][name_field] = {
-							type: 'longtext',
-							null: true,
-							param: data_param
-						}
-					}
+
 
 
 				})
